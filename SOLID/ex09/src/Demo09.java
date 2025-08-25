33b6feb3410331cac7615a004b7edd8d555eb3cf
Harsh-svg988 <harshprabhakar305@gmail.com>
2025-08-26 01:16:25 +0530

    ex09

--- SOLID/ex09/src/Demo09.java
+++ SOLID/ex09/src/Demo09.java
@@ -1,5 +1,9 @@
 public class Demo09 {
     public static void main(String[] args) {
-        new OrderController().create("ORD-1");
+        // choose SQL or in-memory implementation at runtime
+        OrderRepository repo = new SqlOrderRepository();
+        OrderController controller = new OrderController(repo);
+
+        controller.create("ORD-1");
     }
 }
