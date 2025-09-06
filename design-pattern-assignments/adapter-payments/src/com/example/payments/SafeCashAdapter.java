7dd14cf080660849c8fdff5be91767d623a84411
Harsh-svg988 <harshprabhakar305@gmail.com>
2025-09-07 04:00:21 +0530

    adapter-payments

---
+++ design-pattern-assignments/adapter-payments/src/com/example/payments/SafeCashAdapter.java
@@ -0,0 +1,19 @@
+package com.example.payments;
+
+public class SafeCashAdapter implements PaymentGateway {
+    private SafeCashClient client;
+
+    public SafeCashAdapter(SafeCashClient client) {
+        this.client = client;
+    }
+
+
+    @Override
+    public String charge(String customerId, int amountCents) {
+
+        SafeCashPayment payment = client.createPayment( amountCents,customerId);
+        return payment.confirm();
+    }
+
+    
+}
